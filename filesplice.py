3e1d798cd1667053bd065b3d45be3e52e04857d6
Ashutosh Pandey <ashutosh.pandey@gmail.com>
2016-11-18 07:40:33 +0530

    Changed tabs to spaces

--- filesplice.py
+++ filesplice.py
@@ -5,14 +5,14 @@ import os.path
 from itertools import islice
 
 with open("RollNo-DOB-2.csv", 'rb') as infh:
-	reader = csv.reader(infh)
-	records = 0
+    reader = csv.reader(infh)
+    records = 0
 
-	for row in reader:
-		records = records + 100000
-		filename = "RollNo-DOB-2-" + str(records) + ".csv"
-		filename = os.path.join('/Users/Ashutosh/Desktop/', filename)
-		with open (filename, 'wb') as outfh:
-			writer = csv.writer(outfh)
-			writer.writerow(row)
-			writer.writerows(islice(reader, 100000))
+    for row in reader:
+        records = records + 100000
+        filename = "RollNo-DOB-2-" + str(records) + ".csv"
+        filename = os.path.join('/Users/Ashutosh/Desktop/', filename)
+        with open(filename, 'wb') as outfh:
+            writer = csv.writer(outfh)
+            writer.writerow(row)
+            writer.writerows(islice(reader, 100000))
